568f35f89380f41b42692dc71358a03873bcf395
LeonHammerla <leon.hammerla@gmx.de>
2022-02-21 15:46:00 +0100

    added tiger corpus models

--- training_scripts/train_crf2o.sh
+++ training_scripts/train_crf2o.sh
@@ -4,5 +4,8 @@
 
 
 # gsd
-/home/stud_homes/s5935481/work3/venv/bin/python3.8 -m supar.cmds.crf2o_dep train --train /home/stud_homes/s5935481/work3/data/de_gsd-ud-train.conllu --dev /home/stud_homes/s5935481/work3/data/de_gsd-ud-dev.conllu --test /home/stud_homes/s5935481/work3/data/de_gsd-ud-test.conllu --embed /home/stud_homes/s5935481/work3/data/embeddings/lemma_kom_clean.txt --n-embed 100 -b -d 0 -p /home/stud_homes/s5935481/work3/models/crf2o_dep_de_gsd -f char -c /home/stud_homes/s5935481/work3/data/configs/ptb.crf2o.dep.lstm.char.ini --proj --mbr
+# /home/stud_homes/s5935481/work3/venv/bin/python3.8 -m supar.cmds.crf2o_dep train --train /home/stud_homes/s5935481/work3/data/de_gsd-ud-train.conllu --dev /home/stud_homes/s5935481/work3/data/de_gsd-ud-dev.conllu --test /home/stud_homes/s5935481/work3/data/de_gsd-ud-test.conllu --embed /home/stud_homes/s5935481/work3/data/embeddings/lemma_kom_clean.txt --n-embed 100 -b -d 0 -p /home/stud_homes/s5935481/work3/models/crf2o_dep_de_gsd -f char -c /home/stud_homes/s5935481/work3/data/configs/ptb.crf2o.dep.lstm.char.ini --proj --mbr
+
+# tiger
+/home/stud_homes/s5935481/work3/venv/bin/python3.8 -m supar.cmds.crf2o_dep train --train /home/stud_homes/s5935481/work3/data/tiger_train.conllu --dev /home/stud_homes/s5935481/work3/data/tiger_dev.conllu --test /home/stud_homes/s5935481/work3/data/tiger_test.conllu --embed /home/stud_homes/s5935481/work3/data/embeddings/lemma_kom_clean.txt --n-embed 100 -b -d 0 -p /home/stud_homes/s5935481/work3/models/crf2o_dep_de_tiger -f char -c /home/stud_homes/s5935481/work3/data/configs/ptb.crf2o.dep.lstm.char.ini --proj --mbr
 
